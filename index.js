6c78eec43fff64106c82acc1920dacf202bd66ed
KARLALOPEZWDFTJAN2023 <121774234+KARLALOPEZWDFTJAN2023@users.noreply.github.com>
2023-01-12 09:13:34 -0600

    Terminado

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // Iteration 1: Names and Input
+const hacker1 = 'Karla';
+console.log(`The drivers name is "${hacker1}"`)
 
+const hacker2 = 'Claudia';
+console.log(`The navigator name is "${hacker2}"`)
 
 // Iteration 2: Conditionals
+let driver="Karla";
+let navigator="Claudia";
+if(driver.length>navigator.length){
+  console.log("The driver has the longest name, it has", driver.length +"characters")
+}if(driver.length<navigator.length){
+console.log("It seems that the navigator has the longest name, it has",navigator.length +
+ " characters.")
+}else{
+  console.log("Wow, you both have equally long names, XX characters!")
+}
+
 
 
 // Iteration 3: Loops
+console.log(hacker1.toUpperCase().split("")); 
+let str = "drawer";
+let reversed = "re";
+
+for (let i = hacker1.length - 1; i >= 0; i--) {
+  reversed += hacker1[i];
+}
+console.log(for)
+
+
+
+
